f25a316465475eaa43e3f2362cc56eb7a749a259
MMolik <molikos@o2.pl>
2024-05-19 11:10:46 +0200

    App working

--- src/components/Contacts/contacs.jsx
+++ src/components/Contacts/contacs.jsx
@@ -65,4 +65,3 @@ Contacts.propTypes = {
     })
   ).isRequired,
 };
-
---
+++ src/redux/store.js
@@ -0,0 +1,27 @@
+import { configureStore } from '@reduxjs/toolkit';
+import { persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import { contactsReducer } from './contactsSlice';
+import { filterReducer } from './filterSlice';
+
+const contactsPersistConfig = {
+  key: 'contacts',
+  storage,
+};
+
+const store = configureStore({
+  reducer: {
+    contacts: persistReducer(contactsPersistConfig, contactsReducer),
+    filter: filterReducer,
+  },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+const persistor = persistStore(store);
+
+export { store, persistor };
